6298bc1471224722f5be5dc947ae94700022c38d
Sandeep-XD <sanbofficial@gmail.com>
2025-03-13 16:27:39 +0530

    Update TodoList.jsx fix logic and bugs . added functionality
    fixed logical bugs . preview before uncommenting the comment lines .

--- client/src/components/list/TodoList.jsx
+++ client/src/components/list/TodoList.jsx
@@ -1,19 +1,22 @@
 import React, { useState } from 'react'
 import { useTodo } from '../../contexts/TodoContext'
 const TodoList = (todo) => {
-  const { todos, deleteTodo , toggleTodo } = useTodo()
-  const [toggle , setToggle] = useState(false)
-
+  const { deleteTodo , toggleTodo, updateTodo } = useTodo()
+//const [todo , setTodo] = useState({}) // maybe delete
+//const handleUpdate = (e)=>{ updateTodo(todo.id , {id:todo.id,task:e.target.value,...todo}) } //fixed logic
+//const [editable, setEditable] = useState(false)
   const handleToggle = ()=>{
-    setToggle((prev)=>!prev)
+    setEditable((prev)=>!prev)
   }
   return (
     <div className='bg-prime flex rounded-sm p-1'>
         <form className='container gap-2 justify-between'>
           <input 
               type='checkbox'
-              checked = {toggle}
+              checked = {editable}
+						//checked = {todo.completed}
               onChange={handleToggle}
+
             />
           <input 
               className='todo-input'
@@ -21,9 +24,10 @@ const TodoList = (todo) => {
               placeholder={todo.task}
               value={todo.task}
               onChange={(e)=> e.target.value}
+//						onChange={handleUpdate}
             />
           <div className="btn-container">
-              <button className='todo-input-btn' type="submit">edit</button>
+              <button className='todo-input-btn' type="button" onClick={()=>setEditable((prev)=>!prev)} >{editable? "ok":"edit"}</button> // fixed logic 
               <button className='todo-input-btn' type="submit">delete</button>
           </div>
         </form>
